ebd77bc21d48143a097046203fa2835ea1b31923
michael-nmg <michael.nmg@gmail.com>
2016-04-18 21:14:58 +0300

    static def

--- find_obj.rb
+++ find_obj.rb
@@ -1,92 +1,90 @@
 #encoding: utf-8
-
-print "Enter find object: "
-find_obj = gets.strip
-
 Dir.chdir "/home/micahel-nmg/Projects"
 
 class Search
-attr_reader :find_obj, :arr
+  attr_reader :find_obj, :arr
 
-def initialize (find_obj)
-  @find_obj = find_obj
-end
-
-def folders_environment
-  arr = Dir.entries(Dir.pwd)
-  arr.delete(".")
-  arr.delete("..")
-  arr.delete_if { |i|
-    Dir.exist?(i) == false
-  }
-
-  return false if arr.count == 0
-  arr
-
-  rescue Errno::EACCES
-    return false
-end
-
-def include_obj
-  Dir.entries(Dir.pwd).each {|i|
-    return true if i.include?(@find_obj)
-  }
-
-  rescue Errno::EACCES
-    return false
-end
-
-def open_folder(folder)
-  if Dir.pwd != "/"
-    Dir.chdir(Dir.pwd + "/#{folder}")
-  else
-    Dir.chdir(Dir.pwd + "#{folder}/")
+  def self.enter_object
+    print "Enter find object: "
+    @find_obj = gets.strip
   end
 
-  rescue Errno::EACCES
-    return false
-end
+  def self.folders_environment
+    arr = Dir.entries(Dir.pwd)
+    arr.delete(".")
+    arr.delete("..")
+    arr.delete_if { |i|
+      Dir.exist?(i) == false
+    }
 
-def close_folder
-  Dir.chdir(Dir.pwd + "/..")
-end
+    return false if arr.count == 0
+    arr
 
-def add_adress
-  Dir.entries(Dir.pwd).each_with_index { |i, k|
-    print "File is found: > #{Dir.pwd}/#{Dir.entries(Dir.pwd)[k]}\n" if i.include?(@find_obj)
-  }
-end
-
-def status_bar
-  if Dir.pwd.length <= 90
-    print Dir.pwd
-    print "\r" + " " * Dir.pwd.length + "\r" 
+    rescue Errno::EACCES
+      return false
   end
-end
 
-def search (folders)
-  add_adress
-  folders.each { |i|
-    if open_folder(i)
-    status_bar
-      if folders_environment == false && include_obj == true
-        add_adress
-        close_folder
-      elsif folders_environment == false && include_obj == false || folders_environment == false && include_obj.is_a?(Array)
-        close_folder
-      elsif folders_environment.is_a?(Array) && include_obj == true
-        add_adress
-        search(folders_environment)
-        close_folder
-      elsif folders_environment.is_a?(Array) && include_obj == false || folders_environment.is_a?(Array) && include_obj.is_a?(Array)
-        search(folders_environment)
-        close_folder
-      end
+  def self.include_obj
+    Dir.entries(Dir.pwd).each {|i|
+      return true if i.include?(@find_obj)
+    }
+
+    rescue Errno::EACCES
+      return false
+  end
+
+  def self.open_folder(folder)
+    if Dir.pwd != "/"
+      Dir.chdir(Dir.pwd + "/#{folder}")
+    else
+      Dir.chdir(Dir.pwd + "#{folder}/")
     end
-  }
-end
+
+    rescue Errno::EACCES
+      return false
+  end
+
+  def self.close_folder
+    Dir.chdir(Dir.pwd + "/..")
+  end
+
+  def self.add_adress
+    Dir.entries(Dir.pwd).each_with_index { |i, k|
+      print "File is found: > #{Dir.pwd}/#{Dir.entries(Dir.pwd)[k]}\n" if i.include?(@find_obj)
+    }
+  end
+
+  def self.status_bar
+    if Dir.pwd.length <= 90
+      print Dir.pwd
+      print "\r" + " " * Dir.pwd.length + "\r" 
+    end
+  end
+
+  def self.search (folders)
+    add_adress
+    folders.each { |i|
+      if open_folder(i)
+      status_bar
+        if folders_environment == false && include_obj == true
+          add_adress
+          close_folder
+        elsif folders_environment == false && include_obj == false || folders_environment == false && include_obj.is_a?(Array)
+          close_folder
+        elsif folders_environment.is_a?(Array) && include_obj == true
+          add_adress
+          search(folders_environment)
+          close_folder
+        elsif folders_environment.is_a?(Array) && include_obj == false || folders_environment.is_a?(Array) && include_obj.is_a?(Array)
+          search(folders_environment)
+          close_folder
+        end
+      end
+    }
+  end
 
 end 
 
-s = Search.new(find_obj)
-s.search(s.folders_environment)
+Search.enter_object
+Search.search(Search.folders_environment)
+
