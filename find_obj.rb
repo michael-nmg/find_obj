a750a9df44ff6cdbb05fbf9dee7efcca0d996beb
michael-nmg <michael.nmg@gmail.com>
2016-04-22 12:37:39 +0300

    update + rubocop

--- find_obj.rb
+++ find_obj.rb
@@ -1,90 +1,87 @@
-#encoding: utf-8
-Dir.chdir "/home/micahel-nmg/Projects"
+Dir.chdir '/home/micahel-nmg/Projects'
 
 class Search
-  attr_reader :find_obj, :arr
+  attr_reader :find_obj
 
+  # function: enter name for search
   def self.enter_object
-    print "Enter find object: "
+    print 'Enter find object: '
     @find_obj = gets.strip
   end
 
-  def self.folders_environment
-    arr = Dir.entries(Dir.pwd)
-    arr.delete(".")
-    arr.delete("..")
-    arr.delete_if { |i|
-      Dir.exist?(i) == false
-    }
-
-    return false if arr.count == 0
-    arr
-
-    rescue Errno::EACCES
-      return false
-  end
-
-  def self.include_obj
-    Dir.entries(Dir.pwd).each {|i|
-      return true if i.include?(@find_obj)
-    }
-
-    rescue Errno::EACCES
-      return false
-  end
-
   def self.open_folder(folder)
-    if Dir.pwd != "/"
+    if Dir.pwd != '/'
       Dir.chdir(Dir.pwd + "/#{folder}")
     else
       Dir.chdir(Dir.pwd + "#{folder}/")
     end
+  rescue Errno::EACCES
+    return false
+  end
 
+  def self.close_folder
+    Dir.chdir(Dir.pwd + '/..')
+  end
+
+  def self.folders_content
+    arr = Dir.entries(Dir.pwd)
+    arr.delete('.')
+    arr.delete('..')
+    arr.delete_if do |i|
+      Dir.exist?(i) == false
+    end
+    return false if arr.count == 0
+    arr
     rescue Errno::EACCES
       return false
   end
 
-  def self.close_folder
-    Dir.chdir(Dir.pwd + "/..")
+  def self.include_obj?
+    Dir.entries(Dir.pwd).each do |i|
+      return true if i.include?(@find_obj)
+    end
+    return false
+    rescue Errno::EACCES
+      return false
   end
 
   def self.add_adress
-    Dir.entries(Dir.pwd).each_with_index { |i, k|
-      print "File is found: > #{Dir.pwd}/#{Dir.entries(Dir.pwd)[k]}\n" if i.include?(@find_obj)
-    }
+    Dir.entries(Dir.pwd).each_with_index do |i, k|
+      if i.include?(@find_obj)
+        print "File is found: > #{Dir.pwd}/#{Dir.entries(Dir.pwd)[k]}\n"
+      end
+    end
   end
 
   def self.status_bar
     if Dir.pwd.length <= 90
       print Dir.pwd
-      print "\r" + " " * Dir.pwd.length + "\r" 
+      print "\r" + ' ' * Dir.pwd.length + "\r"
     end
   end
 
-  def self.search (folders)
+  def self.search(folders)
     add_adress
-    folders.each { |i|
+    folders.each do |i|
       if open_folder(i)
-      status_bar
-        if folders_environment == false && include_obj == true
+        if folders_content == false && include_obj? == true
           add_adress
           close_folder
-        elsif folders_environment == false && include_obj == false || folders_environment == false && include_obj.is_a?(Array)
+        elsif folders_content == false && include_obj? == false
           close_folder
-        elsif folders_environment.is_a?(Array) && include_obj == true
+        elsif folders_content.is_a?(Array) && include_obj? == true
           add_adress
-          search(folders_environment)
+          search(folders_content)
           close_folder
-        elsif folders_environment.is_a?(Array) && include_obj == false || folders_environment.is_a?(Array) && include_obj.is_a?(Array)
-          search(folders_environment)
+        elsif folders_content.is_a?(Array) && include_obj? == false
+          search(folders_content)
           close_folder
         end
       end
-    }
+    end
   end
-
-end 
+end
 
 Search.enter_object
-Search.search(Search.folders_environment)
+Search.search(Search.folders_content)
 
